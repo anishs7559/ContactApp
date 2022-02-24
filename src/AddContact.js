1f11519bb2b218e3d452193a00bfb4c6621521c0
anishs7559 <anishs7559@gmail.com>
2022-02-24 22:36:50 +0530

    created multi inputs and delete button

--- src/AddContact.js
+++ src/AddContact.js
@@ -4,15 +4,20 @@ const AddContact = () => {
   const [input,setInput]=useState('')
   const[contact,setContact]=useState([])
   const [state,setState]=useState('')
-  const[value,setValue]=useState([])
+  
   const handleSubmit=(e)=>{
    e.preventDefault()
-   setContact([...contact,{id:new Date().getTime(),text:input,complete:false}])
+   setContact([...contact,{id:new Date().getTime(),name:input,complete:false,email:state}])
    setInput('')
-   setValue([...value,{id:new Date().getTime(),text:state,complete:false}])
+   
    setState('')
 
   }
+ function handleDelete(id){
+  const update=[...contact].filter((contact)=>contact.id!==id)
+  setContact(update)
+ }
+  
   return (
     <div>
       
@@ -20,20 +25,18 @@ const AddContact = () => {
      
         <form onSubmit={handleSubmit} >
         <label>name</label><br />
-          <input value={input} onChange={(e)=>setInput(e.target.value)}/>
+          <input  value={input} onChange={(e)=>setInput(e.target.value)}/>
           <br />
           <label >phone number</label><br />
-          <input value={state} onChange={(e)=>setState(e.target.value)} />
+          <input type={'number'} value={state} onChange={(e)=>setState(e.target.value)} />
           <br/> <button>submit</button> 
         </form> </div>
   
     <div className='contactList'>
-      {contact.map((value)=>{return(<div key={value.id}><h3>{value.text}</h3></div>)})}
-      {value.map((value)=>{return(<div>{value.text}</div>)})}
-      </div>    
-     
-    
-    </div>)
+      {contact.map((value)=>{return(<div key={value.id}><h3>{value.name}</h3> <h3>{value.email}</h3> <button onClick={()=>handleDelete(value.id)}>delete</button></div>)})}
+      </div>
+    </div>
+    )
 }
 
 export default AddContact
