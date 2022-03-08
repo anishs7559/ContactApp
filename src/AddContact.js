05374254502678ead2ac3e421c4ad31721efc9b5
anishs7559 <anishs7559@gmail.com>
2022-03-08 12:35:48 +0530

    editing button

--- src/AddContact.js
+++ src/AddContact.js
@@ -1,9 +1,12 @@
 import React,{useState} from 'react'
+import './App.css'
 
 const AddContact = () => {
   const [input,setInput]=useState('')
   const[contact,setContact]=useState([])
   const [state,setState]=useState('')
+  const [edit,setEdit]=useState('')
+  const[value,setValue]=useState('')
   
   const handleSubmit=(e)=>{
    e.preventDefault()
@@ -16,6 +19,10 @@ const AddContact = () => {
  function handleDelete(id){
   const update=[...contact].filter((contact)=>contact.id!==id)
   setContact(update)
+ }
+ const handleEdit=(id)=>{
+   setContact([...contact,{name:edit,email:value}])
+
  }
   
   return (
@@ -30,10 +37,15 @@ const AddContact = () => {
           <label >phone number</label><br />
           <input type={'number'} value={state} onChange={(e)=>setState(e.target.value)} />
           <br/> <button>submit</button> 
-        </form> </div>
+        </form>
+        <input type="text" onChange={(e)=>setEdit(e.target.value)} />
+        <input type="text" onChange={(e)=>setValue(e.target.value)}/> 
+        <button>submit</button> </div>
+
   
-    <div className='contactList'>
-      {contact.map((value)=>{return(<div key={value.id}><h3>{value.name}</h3> <h3>{value.email}</h3> <button onClick={()=>handleDelete(value.id)}>delete</button></div>)})}
+    <div>
+      {contact.map((value)=>{return(<div  key={value.id}><h3>{value.name}</h3>
+       <h3>{value.email}</h3> <button onClick={()=>handleDelete(value.id)}>delete</button> <button onClick={handleEdit(value.id)}>edit</button></div>)})}
       </div>
     </div>
     )
