c277cfaeb17083ccc1b0057125a965c456b66920
anishs7559 <anishs7559@gmail.com>
2022-03-17 13:45:22 +0530

    created an edit

--- src/AddContact.js
+++ src/AddContact.js
@@ -5,6 +5,7 @@ const AddContact = () => {
   const [input,setInput]=useState('')
   const[contact,setContact]=useState([])
   const [state,setState]=useState('')
+  const [editvalue,setEditvalue]=useState(null)
   const [edit,setEdit]=useState('')
   const[value,setValue]=useState('')
   
@@ -20,11 +21,7 @@ const AddContact = () => {
   const update=[...contact].filter((contact)=>contact.id!==id)
   setContact(update)
  }
- const handleEdit=(id)=>{
-   setContact([...contact,{name:edit,email:value}])
 
- }
-  
   return (
     <div>
       
@@ -35,17 +32,20 @@ const AddContact = () => {
           <input  value={input} onChange={(e)=>setInput(e.target.value)}/>
           <br />
           <label >phone number</label><br />
-          <input type={'number'} value={state} onChange={(e)=>setState(e.target.value)} />
+          <input value={state} onChange={(e)=>setState(e.target.value)} />
           <br/> <button>submit</button> 
-        </form>
-        <input type="text" onChange={(e)=>setEdit(e.target.value)} />
-        <input type="text" onChange={(e)=>setValue(e.target.value)}/> 
-        <button>submit</button> </div>
+        </form> 
+       </div>
 
   
     <div>
-      {contact.map((value)=>{return(<div  key={value.id}><h3>{value.name}</h3>
-       <h3>{value.email}</h3> <button onClick={()=>handleDelete(value.id)}>delete</button> <button onClick={handleEdit(value.id)}>edit</button></div>)})}
+      {contact.map((value)=>{return(<div  key={value.id}>
+        {editvalue===value.id?(<form><label>name</label><br />
+        <input/> <br /> <label>phn</label> <br /><input/></form>):
+        (<form><h3>{value.name}</h3><h3>{value.email}</h3></form>)}
+      
+        <button onClick={()=>handleDelete(value.id)}>delete</button> 
+        <button onClick={()=>setEditvalue(value.id)}>edit</button></div>)})}
       </div>
     </div>
     )
